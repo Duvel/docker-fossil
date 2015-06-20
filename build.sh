bdcaa03ee2ea5f840a186e79c859696fb10a2e0c
remco <remco@noemail.net>
2015-06-20 17:26:16 +0000

    Updated build script
    FossilOrigin-Name: 63cf4fafd2a2ed99380e112609ef30c0e37f70e8a8fb18e8cc2af57281ee69fc

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-#!/bin.sh
-docker build -t rpi-fossil-builder . && docker run rpi-fossil-builder | docker build -t duvel/rpi-fossil -
+#!/bin/sh
+docker build -t duvel/rpi-fossil-builder . && docker run duvel/rpi-fossil-builder | docker build -t duvel/rpi-fossil -
